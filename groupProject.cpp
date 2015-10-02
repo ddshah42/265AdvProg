6200d4ba597b98fcfc3cd5867b07791884da9ddf
Bernin A. Uben <BerninUben@icloud.com>
2015-10-02 00:03:50 -0400

    finished the simple logic for the bFast calcs

--- groupProject.cpp
+++ groupProject.cpp
@@ -4,53 +4,73 @@ using namespace std;
 double dailyEats(double, double, double);
 
 int main() {
-    dailyEats(7,7,2)
+    dailyEats(7,7,2);
 }
 
 
 double dailyEats(double departureTime, double arrivalTime, double tripDays){
-    double total, bAmount, lAmount, dAmount, debted, covered, totalDebt;
-
+    double total, cost, bExpense, covered, totalDebt;
+    String answer = "";
+            
     //program should take the monitary amount of each meal eaten.
     for(double i = 0; i > tripDays; i++){
-        String answer = "";
-        cout << "Did you have Breakfast on day " << i << " ? [Y/N]:\n";
+        cout << "Did you have breakfast on day " << i << "? [Y/N]:\n";
         cin << answer;
-
+        
         while(answer != "Y" || answer != "y" || answer != "N" || answer != "n"){
             cout << "\nI didnt quite get that. Did you have Breakfast on day " << i << " ? [Y/N]: ";
             cin >> answer;
         }
-
+        
+        //Starts the Yes portion of the loop
         if(answer == "Y" || answer == "y"){
             cout << "\nHow much did it cost?: ";
-            cin >> bAmount;
+            cin >> cost;
             
-            if(bAmount > 9){
-                debted = bAmount - 9;
-                totalDebt = totalDebt + debted;
-                covered = 9;
-                
-                
-                if(covered > 0){
-                cout << "Expenses covered: " <<  <<  << debted << endl;
-                } else {
-                    
-                }
-                
-            } else if(bAmount < 9){
-                cout << "Expenses covered: " << 
+            /* IDEA: Itterate in an array for each item so add an array of:
+            ["breakfast", "Lunch", "Dinner"],
+            and use one amount variable(instead of b/l/d amount) to itterate and keep track. 
+            
+            Do the same with the amount limits in a different array that has [9,12,16]*/
+            
+            while(cost < 0){
+                cout << "\nReally? So you were paid to eat there?\n";
+                cout << "Please CORRECTLY enter the amount spent, value more than $0: ";
+                cin << cost;
             }
-        }else if(answer == "N" || answer == "n"){
+            
+            /*if the cost of breakfast is greater than 9, add $9 to covered,
+            add the remainder to totalDebt*/
+            if(cost >= 9){
+                if(cost == 9){
+                    cout << "\nBreakfast expenses covered: $9\n";
+                } else if(cost > 9){
+                    double debt = 0;
+                    
+                    debt = cost - 9;
+                    totalDebt = totalDebt + debt;
+                    bExpense = 9;
+                    covered = covered + bExpense;
+                    
+                    if(debt == 0){
+                        cout << "\nBreakfast expenses covered: "<<bExpense<<".\nDebt: "<<debt<<".\n";
+                    }
+                }
+            } else if(cost < 9){
+                covered = covered + cost;
+                cout << "Expenses covered: " << cost << endl;
+            }
+        //Starts the NO portion of the loop
+        } else if(answer == "N" || answer == "n"){
             cout << "Really? You should have at least something in the morning!"
         }
         
-        
         //resets the answer variable so it doesnt carry over to the next question
-        answer = ""
-
-        //cout << "Did you have Lunch? [Y/N]:\n"
-        //cout << "Did you have Dinner? [Y/N]:\n"
+        answer = "";
+        cost = 0;
+        /*     --------------------- LUNCH STAGE ---------------------   */
+        
+        /*     --------------------- DINNER STAGE ---------------------   */
     }
     
     //program should only take the allowable meals
