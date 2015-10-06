80ff872240a8035545d28b1c402a4862913339df
Bernin A. Uben <BerninUben@icloud.com>
2015-10-06 02:10:14 -0400

    finished looping through mealType and eachMax with arrays to shorten code

--- groupProject.cpp
+++ groupProject.cpp
@@ -9,69 +9,82 @@ int main() {
 
 
 double dailyEats(double departureTime, double arrivalTime, double tripDays){
-    double total, cost, bExpense, covered, totalDebt;
-    String answer = "";
-            
-    //program should take the monitary amount of each meal eaten.
-    for(double i = 0; i > tripDays; i++){
-        cout << "Did you have breakfast on day " << i << "? [Y/N]:\n";
-        cin << answer;
-        
-        while(answer != "Y" || answer != "y" || answer != "N" || answer != "n"){
-            cout << "\nI didnt quite get that. Did you have Breakfast on day " << i << " ? [Y/N]: ";
+    double total = 0, cost = 0, covered = 0, totalDebt = 0, trip = 0, bExpense = 0;
+    string answer = "";
+    string mealType[3] = {"breakfast", "lunch", "dinner"};
+    int eachMax[4] = {0, 9, 12, 16};
+    int startDay = 1, mealPos = 0;
+    
+    //tripDays needs to have 1 added to its value for it to work.
+    //The days start at 1 not 0 but the array starts at 0 so were starting it at 1 too
+    tripDays += 1;
+    
+    for(int i=startDay;i < tripDays;i++){
+        for(int x=mealPos;x < 3;x++){
+            cout << "\nDid you have " << mealType[x] << " on day " << i << "? [Y/N]: ";
             cin >> answer;
-        }
-        
-        //Starts the Yes portion of the loop
-        if(answer == "Y" || answer == "y"){
-            cout << "\nHow much did it cost?: ";
-            cin >> cost;
             
-            /* IDEA: Itterate in an array for each item so add an array of:
-            ["breakfast", "Lunch", "Dinner"],
-            and use one amount variable(instead of b/l/d amount) to itterate and keep track. 
-            
-            Do the same with the amount limits in a different array that has [9,12,16]*/
-            
-            while(cost < 0){
-                cout << "\nReally? So you were paid to eat there?\n";
-                cout << "Please CORRECTLY enter the amount spent, value more than $0: ";
-                cin << cost;
-            }
-            
-            /*if the cost of breakfast is greater than 9, add $9 to covered,
-            add the remainder to totalDebt*/
-            if(cost >= 9){
-                if(cost == 9){
-                    cout << "\nBreakfast expenses covered: $9\n";
-                } else if(cost > 9){
-                    double debt = 0;
-                    
-                    debt = cost - 9;
-                    totalDebt = totalDebt + debt;
-                    bExpense = 9;
-                    covered = covered + bExpense;
-                    
-                    if(debt == 0){
-                        cout << "\nBreakfast expenses covered: "<<bExpense<<".\nDebt: "<<debt<<".\n";
-                    }
+            if(answer == "Y" || answer == "y"){
+                cout << "\nHow much did it cost?:";
+                cin >> cost;
+                
+                while(cost < 0){
+                    cout << "Really? So you were paid to eat there?\n";
+                    cout << "Please CORRECTLY enter the amount spent, value more than $0: ";
+                    cin >> cost;
                 }
-            } else if(cost < 9){
-                covered = covered + cost;
-                cout << "Expenses covered: " << cost << endl;
+                
+                if(cost >= eachMax[x+1]){
+                    if(cost == eachMax[x+1]){
+                        covered = covered + eachMax[x+1];
+                        cout <<"\n"<< mealType[x] << " expenses covered: " << eachMax[x+1] << endl;
+                    }else if(cost > eachMax[x+1]){
+                        double debt = 0;
+                        debt = cost - eachMax[x+1];
+                        totalDebt = totalDebt + debt;
+                        bExpense = eachMax[x+1];
+                        covered = covered + bExpense;
+                        
+                        if(debt == 0){
+                            cout << mealType[x] << " expenses covered: $"<<bExpense<<".\n\n";
+                        } else {
+                            cout << mealType[x] << " expenses covered: $"<< bExpense;
+                            cout <<".\ndebt added: "<<debt<<".\n";
+                        }
+                    }
+                }else if(cost < eachMax[x+1]){
+                    covered = covered + cost;
+                    cout <<  mealType[x] << " expenses covered: $" << cost << endl;
+                }
+            } else if(answer == "N" || answer == "n"){
+                cout << "No " << mealType[x] << ", got it. \n";
             }
-        //Starts the NO portion of the loop
-        } else if(answer == "N" || answer == "n"){
-            cout << "Really? You should have at least something in the morning!"
+            
+            answer = "";
+            cost = 0;
         }
-        
-        //resets the answer variable so it doesnt carry over to the next question
-        answer = "";
-        cost = 0;
-        /*     --------------------- LUNCH STAGE ---------------------   */
-        
-        /*     --------------------- DINNER STAGE ---------------------   */
+        startDay++;
     }
+    
+    cout << "\n\nTotal Debt: " << totalDebt << endl;
+    cout << "Food expenses covered: " << covered << endl;
+}
+
+/*        
+    if(answer != "Y" || answer != "y" || answer != "N" || answer != "n"){
+        while(answer != "Y" || answer != "y" || answer != "N" || answer != "n"){
+            cout << "\nI didnt quite get that. ";
+            cout << "Did you have " << mealType[x] << " on day " << i << " ? [Y/N]: ";
+            cin >> answer;
+    } else {
+        cout << "\nOh Okay, ";
+    }
+*/
+    
+    /* IDEA: Itterate in an array for each item so add an array of:
+    ["breakfast", "Lunch", "Dinner"],
+    and use one amount variable(instead of b/l/d amount) to itterate and keep track. 
+    Do the same with the amount limits in a different array that has [9,12,16]*/
     
     //program should only take the allowable meals
     //company allows up to:
@@ -87,4 +100,3 @@ double dailyEats(double departureTime, double arrivalTime, double tripDays){
     //if arrival is before 8am, breakfast is allowed as an expense
     //if arrival is before 1pm, lunch is allowed as an expense
     //if arrival is before 7pm, dinner is allowed as an expense
-}
