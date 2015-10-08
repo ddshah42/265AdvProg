a4782976709a0ce5b325ac4ed6af8ab34f3ad993
Bernin A. Uben <BerninUben@icloud.com>
2015-10-08 15:18:35 -0400

    Finished the program. May clean it up. Fixed the bug. Made covered and totalDebt global variables to bring the values back to main. Made a work-around from lines 88 ---> 91

--- groupProject.cpp
+++ groupProject.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 using namespace std;
 double dailyEats(double, double, double);
+
+double covered = 0, totalDebt = 0;
+
 int main() {
     dailyEats(799,1500,2);
+    cout << "\nwe're back in the main!\n";
+    cout << "\nTotal Debt: $" << totalDebt << "\nFood expenses covered: $" << covered << endl;
 }
+
 double dailyEats(double arrivalTime, double departureTime, double tripDays){
     int i, x, f, mealPos = 0, fDayMealStart = 0, onlyOnce = 0;
     int dMealVal = 3, intTripDays = tripDays;//sets double tripDays to int
-    double cost = 0, covered = 0, totalDebt = 0, bExpense = 0;
+    double cost = 0, bExpense = 0;
     string mealType[3] = {"breakfast", "lunch", "dinner"};
     int mealMax[3] = {9, 12, 16};
     string answer = ""; //tripDays += 1; //tripDays needs to have one added to its value for it to work. The days start at 1 not 0. // If you uncomment this then you need to change the "<=" in "i <= tripDays" to an "<" sign
     for(i = 1;i <= tripDays;i++){
         if(i == intTripDays){
             if(departureTime < 700){
-                mealType[1] = {"0"}; mealType[2] = {"0"};
+                mealType[1] = "0"; mealType[2] = "0";
                 if(onlyOnce == 0){cout << "\n ****** Only breakfast is being covered ****** \n"; onlyOnce++;}
             }else if(departureTime < 1200){
-                mealType[2] = {"0"};
+                mealType[2] = "0";
                 if(onlyOnce == 0){cout << "\n ****** Only breakfast & lunch is being covered ****** \n"; onlyOnce++;}
             }else if(departureTime < 1800){
                 
             } else {
-                mealType[0] = {"0"}; mealType[1] = {"0"}; mealType[2] = {"0"};
+                mealType[0] = "0"; mealType[1] = "0"; mealType[2] = "0";
             }
         }
         for(x = mealPos;x < dMealVal;x++){ //could of reversed this for the departureTime functionallity *Face palm*
@@ -81,7 +87,7 @@ double dailyEats(double arrivalTime, double departureTime, double tripDays){
                         if(answer == "Y" || answer == "y" || answer == "N" || answer == "n"){
                 //COPY OF LINES 48- --> 78 START        //COPY OF LINES 48- --> 78 START
                 //ugly code but I hacked it together for it to work so whatever. Had to do it this way because this crap ass language wont let me do what I wanted to do. It kept throwing an error. so I compressed a copy of lines 48 --> 78 and inserted it here.
-                            if(answer=="Y"||answer=="y"){cout<<"\nHow much did it cost?:";cin>>cost;while(cost<0){cout<<"Really? So you were paid to eat there?\n";cout <<"Please CORRECTLY enter the amount spent, value more than $0: ";cin>>cost;}if(cost>=mealMax[x]){if(cost==mealMax[x]){covered=covered+mealMax[x];cout<<"\n"<<mealType[x]<<"expenses covered: "<< mealMax[x]<<endl;}else if(cost>mealMax[x]){double debt=0;debt=cost-mealMax[x];totalDebt=totalDebt+debt;bExpense=mealMax[x];covered=covered+bExpense;if(debt==0){cout<<mealType[x]<<" expenses covered: $"<<bExpense<<".\n\n";}else{cout<<mealType[x]<< " expenses covered: $"<<bExpense;cout <<".\ndebt added: "<<debt<<".\n";}}}else if(cost<mealMax[x]){covered=covered+cost;cout<<mealType[x]<<" expenses covered: $"<<cost<<endl;}}else if(answer=="N"||answer=="n"){cout<<"No "<< mealType[x]<<", got it. \n";}
+                            if(answer=="Y"||answer=="y"){cout<<"\nHow much did it cost?:";cin>>cost;while(cost<0){cout<<"Really? So you were paid to eat there?\n";cout <<"Please CORRECTLY enter the amount spent, value more than $0: ";cin>>cost;}if(cost>=mealMax[x]){if(cost==mealMax[x]){covered=covered+mealMax[x];cout<<"\n"<<mealType[x]<<"expenses covered: "<< mealMax[x]<<endl;}else if(cost>mealMax[x]){double debt=0;debt=cost-mealMax[x];totalDebt=totalDebt+debt;bExpense=mealMax[x];covered=covered+bExpense;if(debt==0){cout<<mealType[x]<<" expenses covered: $"<<bExpense<<".\n\n";}else{cout<<mealType[x]<<" expenses covered: $"<<bExpense;cout <<".\ndebt added: "<<debt<<".\n";}}}else if(cost<mealMax[x]){covered=covered+cost;cout<<mealType[x]<<" expenses covered: $"<<cost<<endl;}}else if(answer=="N"||answer=="n"){cout<<"No "<< mealType[x]<<", got it. \n";}
                 //COPY OF LINES 48- --> 78 END      //COPY OF LINES 48- --> 78 END
                             break;
                         }
@@ -91,8 +97,6 @@ double dailyEats(double arrivalTime, double departureTime, double tripDays){
             }
         }
     }
-    cout << "\n\nTotal Debt: $" << totalDebt << endl;
-    cout << "Food expenses covered: $" << covered << endl;
 }
 
     //Program should only take the allowable meals
