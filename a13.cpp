29aabb368686ff78247f7fc54903bf0fb9dbfe84
Bernin A. Uben <BerninUben@icloud.com>
2015-12-13 17:27:12 -0500

    Date

--- a13.cpp
+++ a13.cpp
@@ -1,9 +1,5 @@
-//  a13.cpp
-//
 //  Created by Bernin A. Uben on 12/10/15.
 //  Copyright © 2015 Bernin A. Uben. All rights reserved.
-//
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -13,7 +9,6 @@ private:
     int month;
     int day;
     int year;
-    
 public:
     Date(int, int, int);
     void setDate(int, int, int);
@@ -21,90 +16,62 @@ public:
     void dateInformal();
     void dateFormal();
     void dateEurope();
-    
 };
 
 int main(){
     //added main infront of variables for readability and clarity of main Variables
     int mainMonth, mainDay, mainYear;
     bool bVal;
-    //constructor call
-    Date mainDate(1,1,2001);
+    Date mainDate(1,1,2001);//constructor call
     
-    //take user data
-    cout << "Please enter the date.\n";
-    cout << "Enter the month: ";
+    cout << "Please enter a date.\n";//take user data start
+    cout << "Ex:1 = January, 12 = December.\nEnter the month number: ";
     cin >> mainMonth;
-    
     if(mainMonth < 1 || mainMonth > 12){
         bVal = true;
         while(bVal){
-            cout << "hmm... okay, Please enter the CORRECT MONTH VALUE: ";
+            cout << "hmm... okay, Please enter a CORRECT MONTH VALUE: ";
             cin >> mainMonth;
             if(mainMonth >= 1 && mainMonth <= 12){
                 bVal = false;
             }
         }
     }
-    
-    cout << "Enter the day: ";
+    cout << "Enter the day number: ";
     cin >> mainDay;
-    
     if(mainDay < 1 || mainDay > 31){
         bVal = true;
         while(bVal){
-            cout << "hmm... okay, Please enter the CORRECT DAY VALUE: ";
+            cout << "hmm... okay, Please enter a CORRECT DAY VALUE: ";
             cin >> mainDay;
             if(mainDay >= 1 && mainDay <= 31){
                 bVal = false;
             }
         }
     }
-    
     cout << "Enter the year: ";
     cin >> mainYear;
     
-    mainDate.setDate(mainMonth, mainDay, mainYear);
-    cout << endl;cout << endl;
-    mainDate.dateInformal();
-    cout << endl;
-    mainDate.dateFormal();
-    cout << endl;
+    mainDate.setDate(mainMonth, mainDay, mainYear);	cout << endl;
+    mainDate.dateInformal();						cout << endl;
+    mainDate.dateFormal();							cout << endl;
     mainDate.dateEurope();
-    
     return 0;
 }
 
-//Constructor
-Date::Date(int m, int d, int y){
+Date::Date(int m, int d, int y){//Constructor
     month = m;
     day = d;
     year = y;
 }
-
 void Date::setDate(int m, int d, int y){
     month = m;
     day = d;
     year = y;
 }
 
-void Date::dateInformal(){
-    cout << "Informal:  \n" << month << "/" << day << "/" << year << endl;
-}
-
-void Date::dateFormal(){
-    string monthString = getMonth(month);
-    cout << "Formal:    \n" << monthString << " " << day << ", " << year << endl;
-}
-
-void Date::dateEurope(){
-    string monthString = getMonth(month);
-    cout << "EU Format: \n" << day << " " << monthString << " " << year << endl;
-}
-
 string Date::getMonth(int m){
     string month;
-    
     switch(m){
         case 1: month = "January";
             break;
@@ -132,6 +99,17 @@ string Date::getMonth(int m){
             break;
         default: cout << "Invalid Number\n";
     }
-    
     return month;
+}
+
+void Date::dateInformal(){
+    cout << "Informal:  \n" << month << "/" << day << "/" << year << endl;
+}
+void Date::dateFormal(){
+    string monthString = getMonth(month);
+    cout << "Formal:    \n" << monthString << " " << day << ", " << year << endl;
+}
+void Date::dateEurope(){
+    string monthString = getMonth(month);
+    cout << "EU Format: \n" << day << " " << monthString << " " << year << endl;
 }
